8790b5d1a58ca3a0fc182c795376f92031e0d808
Анжелика <lika-2020@yandex.ru>
2023-02-02 21:52:16 +0300

    error react

--- src/components/CenterBlockFilter.js
+++ src/components/CenterBlockFilter.js
@@ -1,14 +1,67 @@
+import { useState } from 'react'
+
 function CenterBlockFilter() {
+  const [filterButtonAuthor, setFilterButtonAuthor] = useState(false)
+  console.log(filterButtonAuthor)
+  const [filterButtonYear, setFilterButtonYear] = useState(false)
+  console.log(filterButtonYear)
+  const [filterButtonGenre, setFilterButtonGenre] = useState(false)
+  console.log(filterButtonGenre)
+
+
+
   return (
     <div>
       <h2 className="centerblock__h2">Треки</h2>
       <div className="centerblock__filter filter">
         <div className="filter__title">Искать по:</div>
-        <div className="filter__button button-author _btn-text">
+        <div
+          className="filter__button button-author _btn-text"
+          onClick={() => setFilterButtonAuthor(!filterButtonAuthor)}
+          role="presentation"
+        >
           исполнителю
         </div>
-        <div className="filter__button button-year _btn-text">году выпуска</div>
-        <div className="filter__button button-genre _btn-text">жанру</div>
+        <div
+          className="filter__button button-year _btn-text"
+          onClick={() => setFilterButtonYear(!filterButtonYear)}
+          role="presentation"
+        >
+          году выпуска
+        </div>
+        <div
+          className="filter__button button-genre _btn-text"
+          onClick={() => setFilterButtonGenre(!filterButtonGenre)}
+          role="presentation"
+        >
+          жанру
+        </div>
+      </div>
+
+      <div className={filterButtonAuthor ? ['modal'] : ['hidden']}>
+        <span>Michael Jackson</span>
+        <span>Michael Jackson</span>
+        <span>Michael Jackson</span>
+        <span>Michael Jackson</span>
+        <span>Michael Jackson</span>
+      </div>
+
+      <div className={filterButtonYear ? ['year'] : ['hidden']}>
+        <label htmlFor="newer">
+          <input id="newer" type="radio" />
+          Более новые
+        </label>
+        <label htmlFor="older">
+          <input id="older" type="radio" />
+          Более старые
+        </label>
+      </div>
+
+      <div className={filterButtonGenre ? ['modal'] : ['hidden']}>
+        <span>Хип-хоп</span>
+        <span>Хип-хоп</span>
+        <span>Хип-хоп</span>
+        <span>Хип-хоп</span>
       </div>
     </div>
   )
